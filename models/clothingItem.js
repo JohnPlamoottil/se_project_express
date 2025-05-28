e1989756d21ec84f4faa5215359e19ec068fa5c4
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-05-28 16:52:28 -0500

    Part 5 The User Schema

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,30 +1,35 @@
 const mongoose = require("mongoose");
 
-const clothingItemSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  weather: {
-    type: String,
-    required: true,
-    enum: ["hot", "warm", "cold"], // optional: enforce values
-  },
-  imageUrl: {
-    type: String,
-    required: [true, "Image URL is required"],
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
-  },
-  likes: [
-    {
+const clothingItemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    weather: {
+      type: String,
+      required: true,
+      enum: ["hot", "warm", "cold"], // optional: enforce values
+    },
+    imageUrl: {
+      type: String,
+      required: [true, "Image URL is required"],
+    },
+    owner: {
       type: mongoose.Schema.Types.ObjectId,
+      required: true,
       ref: "User",
     },
-  ],
-});
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
