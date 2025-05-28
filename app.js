6dae1821e67efe3ec3899b873bb2f87e6c03d6df
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-05-28 15:13:27 -0500

    Part1-2-3 First Steps, ESlint, Entry Point and Hot Reload

---
+++ app.js
@@ -0,0 +1,10 @@
+// entry point
+const express = require("express");
+
+const { PORT = 3001 } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
