865daf95fc8e7c9c837e31846d9f44d9e3de2ea3
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-08-15 15:43:06 -0500

    remove comment out es lint message since i already disable es lint

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-// eslint-disable-next-line import/no-extraneous-dependencies
 const dotenv = require("dotenv");
 const router = require("./routes/index");
 
