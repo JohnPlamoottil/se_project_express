c32a701cefb9441f7c370a9ebd660afa445f67a0
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-08-18 19:49:35 -0500

    add console log err to see what the error 500 is

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 const errorHandler = (err, req, res, next) => {
+  console.log(err);
   const statusCode = err.statusCode || 500;
   const message = err.message || "Error occurred with the server";
 
